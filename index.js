c8e76f59db7532c49917cc759f01d432265c078a
Broivula <elias.koivula1@hotmail.com>
2020-01-24 22:00:41 +0200

    Changed from io to native net
    Because of previous experiences with socket.io and because
    Android and Kotlin doesn't support it (officially), I decided to
    do the socket connection natively

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const net = require('net');
 
-app.get('/', (req, res) => {
-    console.log('working, alright');
+clients = [];
+
+server = net.createServer((socket) => {
+    
+    connection = socket.connect();
+    clients.push(connection);
+    console.log('new connection!');
+
+    socket.on('data', (data) => {
+        console.log('data incoming:');
+        console.log(data.toString());
+        socket.write('holla right back at ya');
+    });
+
+    socket.on('error', (err) => {
+        console.log('something went wrong..');
+        console.log(err);
+    });
+
 });
 
-io.on('connection', (socket) => {
-    socket.emit('action', {data: "data"})
-})
+server.listen(3366, () => {
+    console.log('server started, listening to port 3366');
+});
 
-server.listen(3000, () => {
-    console.log('server running on port 3000');
-})
