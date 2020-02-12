fbcdef60d2194bfd151c4b5d68cc06c086fe447b
Broivula <elias.koivula1@hotmail.com>
2020-02-12 19:52:15 +0200

    trying id inside socket

--- game_file.js
+++ game_file.js
@@ -113,9 +113,9 @@ const updateGameRoomClickAmount = (newAmount, roomNumber) => {
  * @returns {number} The index number of the client.
  */
 const indexOfClientInRoom = (client) => getRoomDataWithSocket(client).clients.map((c) =>
-{ console.log(client);
+{ console.log(client.id);
   console.log('client is ' + client._sockname);
-console.log('and c is: ' + c.username);
+console.log('and c id is: ' + c.socket.id);
   return c.username}).indexOf(client._sockname);
 
 /**
